ebc8fa7e4f7d73bce0d4d1878b6acd33fc61cc52
Forhad-Mac <forhadsiraj@icloud.com>
2026-05-31 19:20:40 +0600

    Tests performed

--- pages/pages/tests.py
+++ pages/pages/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):  # new
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+
+class AboutpageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
+
+
+class ContactpageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/contact/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("contact"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("contact"))
+        self.assertTemplateUsed(response, "contact.html")
